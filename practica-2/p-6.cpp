463185e8bc12c271a1b3c89631d4736990d84700
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-10-05 18:05:47 -0500

    tratar cadenasde caracteres

---
+++ practica-2/p-6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void copiar(string a;string b){
+    b=a;
+}
+string juntar(string a;string b){
+    a+=b;
+    return a;
+}
+int longit(string a){
+    return a.size();
+}
+void comp(string a;string b){
+    if(a==b){cout<<si<<endl;}
+    else{cout<<no<<endl;}
+}
