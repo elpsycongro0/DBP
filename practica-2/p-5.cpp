b2cfc39e5ae3465d7a61fc16266406c60fbb48ed
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-10-05 18:10:20 -0500

    leer txt

---
+++ practica-2/p-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("texto.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+  return 0;
+}
