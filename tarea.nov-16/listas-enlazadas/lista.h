08226173b4e066b756dab4c19eba57afd17837f9
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-11-18 23:17:33 -0500

    Update lista.h

--- tarea.nov-16/listas-enlazadas/lista.h
+++ tarea.nov-16/listas-enlazadas/lista.h
@@ -1,31 +1,38 @@
 #include <bits/stdc++.h>
 #include "Nodo.h"
-using namespace std;
-class lista{
+template <class T>
+class lista {
 private:
-    nodo *primero;
-    nodo *actual;
-    bool vacio(){
-        return(primero==NULL);
-    }
+    Nodo<T> *ultimo;
+    Nodo<T> *primero;
 public:
-    lista(){
-        primero=NULL;
-        actual=NULL;
+    lista(Nodo<T> *i=NULL,Nodo<T> *f=NULL){ultimo=i;primero=f;}
+    ~lista() { while(ultimo) Pop(); }
+    void Push(T t) {
+        Nodo<T> *aux = new Nodo<T>(t, ultimo);
+        if(Vacia()){primero=aux;}
+        ultimo = aux;
     }
-    void insertar(int v){
-        nodo *nuevo = new nodo(v);
-        if(vacio()){primero=nuevo;}
-        else{actual->siguiente=nuevo;}
-        actual=nuevo;
+    T Pop() {
+        T temp(*primero->pT);
+        Nodo<T> *aux = primero;
+        ultimo = aux->siguiente;
+        delete aux;
+        return temp;
     }
-    void mostrar(){
-        nodo *contador=primero;
-        while(contador!=actual){
-            cout<<contador->valor<<endl;
-            contador=contador->siguiente;
-            if(contador==actual){cout<<actual->valor;break;}
-        }
+    bool Vacia() { return ultimo == NULL; }
+    void mostrar() {
+        Nodo<T> *ap=ultimo;
+        while(ap!=primero->siguiente){std::cout<<*ap->pT<<std::endl;ap=ap->siguiente;}
+        std::cout<<"---"<<std::endl;
+    }
+    Nodo<T>* getinicio(){return ultimo;}
+    void apfondo(Nodo<T> *punt){primero->anterior=punt;}
+    void changefondo(Nodo<T> *newfondo){primero=newfondo;}
+    lista<T>* operator +(lista<T> sumando){
+        lista<T> *temp=new lista<T>(sumando.getinicio(),primero);
+        sumando.apfondo(ultimo);
+        return temp;
     }
 
 };
