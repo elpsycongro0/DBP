23fd53a8d2a10a0d53ae17c54ac9079d60e44d47
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-11-18 23:05:49 -0500

    Update pila.h

--- tarea.nov-16/pilas/pila.h
+++ tarea.nov-16/pilas/pila.h
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 #include "Nodo.h"
-using namespace std;
-class pila{
+template <class T>
+class Pila {
 private:
-    nodo *primero;
-    nodo *actual;
-    bool vacio(){
-        return(primero==NULL);
-    }
+    Nodo<T> *inicio;
+    Nodo<T> *fondo;
 public:
-    pila(){
-        primero=NULL;
-        actual=NULL;
+    Pila(Nodo<T> *i=NULL,Nodo<T> *f=NULL){inicio=i;fondo=f;}
+    ~Pila() { while(inicio) Pop(); }
+    void Push(T t) {
+        Nodo<T> *aux = new Nodo<T>(t, inicio);
+        if(Vacia()){fondo=aux;}
+        inicio = aux;
     }
-    void pushpila(int v){
-        nodo *nuevo = new nodo(v);
-        if(vacio()){primero=nuevo;}
-        else{actual->siguiente=nuevo;}
-        actual=nuevo;
+    T Pop() {
+        T temp(*inicio->pT);
+        Nodo<T> *aux = inicio;
+        inicio = aux->anterior;
+        delete aux;
+        return temp;
     }
-    void poppila(){
-        nodo *temp=primero;
-        while(temp->siguiente!=actual){
-            temp=temp->siguiente;
-        }
-        delete(actual);
-        actual=temp;
+    bool Vacia() { return inicio == NULL; }
+    void mostrar() {
+        Nodo<T> *ap=inicio;
+        while(ap!=fondo->anterior){std::cout<<*ap->pT<<std::endl;ap=ap->anterior;}
+        std::cout<<"---"<<std::endl;
     }
-    void mostrar(){
-        nodo *contador=primero;
-        while(contador!=actual){
-            cout<<contador->valor<<endl;
-            contador=contador->siguiente;
-            if(contador==actual){cout<<actual->valor;break;}
-        }
+    Nodo<T>* getinicio(){return inicio;}
+    void apfondo(Nodo<T> *punt){fondo->anterior=punt;}
+    void changefondo(Nodo<T> *newfondo){fondo=newfondo;}
+    Pila<T>* operator +(Pila<T> sumando){
+        Pila<T> *temp=new Pila<T>(sumando.getinicio(),fondo);
+        sumando.apfondo(inicio);
+        return temp;
     }
 
 };
