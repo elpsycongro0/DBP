d23d2e56ab18baeba2bece79ee0c19d11f3ecfc0
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-11-18 23:07:09 -0500

    Update main.cpp

--- tarea.nov-16/pilas/main.cpp
+++ tarea.nov-16/pilas/main.cpp
@@ -2,16 +2,19 @@
 #include "pila.h"
 
 using namespace std;
-
 int main(){
-    pila lst;
-    lst.pushpila(1);
-    lst.pushpila(2);
-    lst.pushpila(3);
-    lst.pushpila(4);
-    lst.poppila();
-    lst.pushpila(5);
-    lst.mostrar();
+    Pila<int> a;
+    Pila<int> b;
+    Pila<int> c;
+    a.Push(13);
+    a.Push(14);
+    a.Push(15);
+    a.Push(16);
+    b.Push(1);
+    b.Push(2);
+    b.Push(3);
+    a.mostrar();
+    b.mostrar();
     return 0;
 
 }
