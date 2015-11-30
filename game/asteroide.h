98662e6cf94bd28ca4b76a4953301a5fe11e9fec
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-11-29 21:12:46 -0500

    Update asteroide.h

--- game/asteroide.h
+++ game/asteroide.h
@@ -2,8 +2,11 @@ class asteroide{
 private:
     int x,y;
 public:
-    asteroide(int);
+    asteroide(int,int);
     void impast();
     void mover();
     void colision(navep&);
+    void reload();
+    int X();
+    int Y();
 };
