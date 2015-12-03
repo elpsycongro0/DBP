8014f40aa53d075ebe0c6cc71eeb1ca7137914f6
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-12-03 01:27:54 -0500

    Update bala.cpp

--- game/bala.cpp
+++ game/bala.cpp
@@ -4,20 +4,20 @@
 #include "funciones.h"
 using namespace std;
 bala::bala(int _x,int _y){
-    x=_x;
-    y=_y;
+	x=_x;
+	y=_y;
 }
 bala::~bala(){
- delete(&x);delete(&y);
+	delete(&x);delete(&y);
 }
 void bala::mover(){
-    gotox(x,y);cout<<" ";
-    if(x<99){x++;}
-    gotox(x,y);cout<<"-";
+	gotox(x,y);cout<<" ";
+	if(x<99){x++;}
+	gotox(x,y);cout<<"-";
 }
 bool bala::fuera(){
-    if(x==99){return true;}
-    else{return false;}
+	if(x==99){return true;}
+	else{return false;}
 }
 int bala::X(){return x;}
 int bala::Y(){return y;}
