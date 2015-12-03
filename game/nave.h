2972efd40ed5ed32358809445aa70848d2130558
RodrigoSanchez <rodrigo.sanchez@ucsp.edu.pe>
2015-12-03 01:42:14 -0500

    Update nave.h

--- game/nave.h
+++ game/nave.h
@@ -1,17 +1,18 @@
 class navep{
 private:
-    int x,y;
-    int corazon;
-    int vidas;
+	int x,y;
+	int corazon;
+	int vidas;
 public:
-    navep(int,int,int,int);
-    void impnav();
-    void bornav();
-    void mover(char);
-    void corazones();
-    void muerte();
-    int X();
-    int Y();
-    bool gameend();
-    friend class asteroide;
+	navep(int,int,int,int);
+	void impnav();
+	void bornav();
+	void mover(char);
+	void corazones();
+	void muerte();
+	int X();
+	int Y();
+	bool gameend();
+	friend class asteroide;
+	friend class extralife;
 };
